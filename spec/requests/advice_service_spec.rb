0098bd1b1e50783c3917277d7d12f1e0b583798c
Megan Gonzales <megan.campbell26@yahoo.com>
2021-07-06 14:12:36 -0600

    Push old work

--- spec/requests/advice_service_spec.rb
+++ spec/requests/advice_service_spec.rb
@@ -9,4 +9,19 @@ describe 'Advice Service' do
     expect(response[:slip]).to have_key(:advice)
     expect(response[:slip]).to have_key(:id)
   end
+
+  it "emails?" do
+    expect(ActionMailer::Base.deliveries.count).to eq(1)
+    email = ActionMailer::Base.deliveries.last
+
+    expect(email.subject).to eq('Nancy Drew is sending you some advice')
+
+    expect(email.reply_to).to eq(['nancydrew@detective.com'])
+
+    expect(email.text_part.body.to_s).to include('Hello Leroy Brown')
+    expect(email.text_part.body.to_s).to include('Nancy Drew has sent you some advice:')
+
+    expect(email.html_part.body.to_s).to include('Hello Leroy Brown')
+    expect(email.html_part.body.to_s).to include('Nancy Drew has sent you some advice:')
+  end
 end
