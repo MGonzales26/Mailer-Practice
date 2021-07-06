df94a0a4c4a367a79adf0907afdf86074012796c
Megan Gonzales <megan.campbell26@yahoo.com>
2021-07-06 14:11:36 -0600

    Push old work

--- app/controllers/advice_controller.rb
+++ app/controllers/advice_controller.rb
@@ -6,5 +6,17 @@ class AdviceController < ApplicationController
 
   def create
     @advice = AdviceGenerator.new
+
+    recipient = params[:friends_email]
+
+    email_info = {
+      user: current_user, 
+      friends: params[:friends_name],
+      message: @advice.message
+    }
+
+    FriendNotifierMailer.inform(email_info, recipient.deliver_now)
+    flash[:notice] = 'Thank you for sending some friendly advice.'
+    redirect_to advice_url
   end
 end
